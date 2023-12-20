1f2c3809fd315f3c59fdb5c66744066a4361400d
PrinceOfCookies <iuploadgmod@gmail.com>
2023-12-20 10:02:39 -0600

    Formatting, Variables, Capitalization, ect

--- propkill_script_loader.lua
+++ propkill_script_loader.lua
@@ -1,40 +1,49 @@
-oink.ui_context("prop-kill-script-loader.lua")
+-- Variables --
+local fileName = "prop-kill-script-loader.lua"
+local fileNameOBF = "prop-kill-script-loader-obfuscated.lua"
+local color = 0x00FFFF00
 
+-- UI Shit --
+oink.ui_context(fileName)
+
+-- Buttons
 oink.ui_button("basic load")
 oink.ui_button("secure load (unstable)")
 oink.ui_button("FAQ")
 
 oink.event_remove("view_render_post", "pksLoader")
 oink.event_listen("view_render_post", "pksLoader", function()
+    if oink.ui_get(fileName, "basic load") then
+        oink.log(color, "runstring error, use secure load.")
+    end
 
-	if oink.ui_get("prop-kill-script-loader.lua", "basic load") then
-		oink.log(0x00FFFF00, "runstring error, use secure load.")
-	end
+    if oink.ui_get(fileName, "secure load (unstable)") then
+        local _CompileString = oink.get_original("_G.CompileString")
 
-	if oink.ui_get("prop-kill-script-loader.lua", "secure load (unstable)") then
-		local _CompileString = oink.get_original("_G.CompileString")
-
-		http.Fetch("https://strw.club/propkill_script_oink_obfuscated.lua", function(body)
-        		local sigmond = _CompileString(body, "propkill_script_oink_obfuscated.lua")
-        		if type(sigmond) == "function" then
-            			setfenv(sigmond, getfenv())
-            			sigmond()
-        		end
-    		end, function(error)
-        		print("possible update, error: " .. error)
-    		end)
-	end
+        http.Fetch("https://strw.club/" .. fileNameOBF, function(body)
+            local sigmond = _CompileString(body, fileNameOBF)
+            if type(sigmond) == "function" then
+                setfenv(sigmond, getfenv())
+                sigmond()
+            end
+        end, function(error)
+            print("Possible update, error: " .. error)
+        end)
+    end
 
 
-	if oink.ui_get("prop-kill-script-loader.lua", "FAQ") then
-		oink.log(0x00FFFF00, "Q: how do i fix crashing?")
-		oink.log(0x00FFFF00, "A: you gotta run the entire script manually. go to the http.Fetch, copy the link and paste it to ur browser. once u downloaded the file, paste the raw file in the loader.")
-		oink.log(0x00FFFF00, "Q: how do i use this script?")
-		oink.log(0x00FFFF00, "A: run the script and you'll have a new context. that context will have a sweet little button called 'instructions'")
-		oink.log(0x00FFFF00, "Q: will this script stay free")
-		oink.log(0x00FFFF00, "A: yes")
-		oink.log(0x00FFFF00, "Q: it doesnt work. where do i get support?")
-		oink.log(0x00FFFF00, "A: dm me on discord (lifeline4603) with this (also in ur console): pks_loader_1.1, pks_3.0, [QUESTION/BUG/ISSUE/SUGGESTION/ETC HERE]")
-		print("pks_loader_1.1, pks_3.0, [QUESTION/BUG/ISSUE/SUGGESTION/ETC HERE]")
-	end
+    if oink.ui_get(fileName, "FAQ") then
+        oink.log(color, "Q: How do i fix crashing?")
+        oink.log(color,
+            "A: You gotta run the entire script manually. go to the http.Fetch, copy the link and paste it to ur browser. once u downloaded the file, paste the raw file in the loader.")
+        oink.log(color, "Q: How do i use this script?")
+        oink.log(color,
+            "A: Run the script and you'll have a new context. that context will have a sweet little button called 'instructions'")
+        oink.log(color, "Q: Will this script stay free")
+        oink.log(color, "A: Yes")
+        oink.log(color, "Q: It doesnt work. where do i get support?")
+        oink.log(color,
+            "A: Dm me on discord (lifeline4603) with this (also in ur console): pks_loader_1.1, pks_3.0, [QUESTION/BUG/ISSUE/SUGGESTION/ETC HERE]")
+        print("pks_loader_1.1, pks_3.0, [QUESTION/BUG/ISSUE/SUGGESTION/ETC HERE]")
+    end
 end)
